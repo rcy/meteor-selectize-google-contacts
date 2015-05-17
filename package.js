d890eee03a73542b1b407c0b043dfcb14b18e27d
Ryan Yeske <rcyeske@gmail.com>
2015-05-16 17:00:06 -0700

    accept value

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'rcy:selectize-google-contacts',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Input for multiple google contacts using selectize',
   // URL to the Git repository containing the source code for this package.
